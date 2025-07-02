4df010f05edf8b8cbe807987941b2f764427a380
kocherginVA <meduzvk@yandex.ru>
2025-07-02 11:11:14 +0300

    NameOfElement interface created

--- src/main/java/com/teststore/pages/HomePage.java
+++ src/main/java/com/teststore/pages/HomePage.java
@@ -1,4 +1,15 @@
 package com.teststore.pages;
 
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+
 public class HomePage extends AbstractPage {
+
+    @NameOfElement("Login or register button (header)")
+    @FindBy(xpath = "//a[text()='Login or register']")
+    public SelenideElement buttonLoginOrRegister;
+
+    @NameOfElement("Specials button (header)")
+    @FindBy(xpath = "//li[@data-id='menu_specials' and @class='dropdown']//span[@class='menu_text' and contains(text(),'Specials')]")
+    public SelenideElement buttonSpecials;
 }
